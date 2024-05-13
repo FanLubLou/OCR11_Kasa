8b5eddf10c2976899647a80e14a92c2ac05142ae
FanLubLou <jaclermont@hotmail.com>
2024-05-13 17:17:22 +0200

    Etape3_B Implementation du rooting dans le fichier App.js et creation de fichiers vides dans chaque dossier component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Home } from "./pages/Home.jsx";
+import { About } from "./pages/About/About.jsx";
+import { Accommodation } from "./pages/Accommodation/Accommodation.jsx";
+import { Error } from "./pages/Error/Error.jsx";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/accommodation/:id" element={<Accommodation />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
