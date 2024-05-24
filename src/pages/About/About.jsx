3290c3cd3cbcafbdc8f1fa61a3e1d0a169834997
FanLubLou <jaclermont@hotmail.com>
2024-05-24 10:42:09 +0200

    Etape4-C Mise à disposition dans le composant aout d une liste de propriétés pour le composant collapse

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,13 +5,35 @@ import Footer from '../../components/Footer/Footer'
 import AboutImage from '../../assets/banner_about.png'
 
 export default function About() {
+
+  const aboutDatas = [
+    {
+      id: 0,
+      title: "Fiabilité",
+      content: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+    },
+    {
+      id: 1,
+      title: "Respect",
+      content: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+    },
+    {
+      id: 2,
+      title: "Service",
+      content: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+    },
+    {
+      id: 3,
+      title: "Sécurité",
+      content: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+    },
+  ]
+
   return (
       <div className="about">
           <Header />
           <Banner image={AboutImage} />
-          <Footer />
-
-      
+          <Footer />      
     </div>
   )
 }
